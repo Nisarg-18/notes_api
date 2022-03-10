2d40147fe631b9d7478b4065f8b7d5e460440e07
Nisarg <nisargs2001@gmail.com>
2022-03-10 16:36:37 +0530

    added CRUD functionality

--- notes/api/urls.py
+++ notes/api/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('',view=views.getRoutes)
+    path('',view=views.getRoutes),
+    path('notes/',view=views.getNotes),
+    path('notes/create/',view=views.createNote),
+    path('notes/<str:pk>/update/',view=views.updateNote),
+    path('notes/<str:pk>/',view=views.getNote),
+    path('notes/<str:pk>/delete',view=views.deleteNote),
+
 ]
